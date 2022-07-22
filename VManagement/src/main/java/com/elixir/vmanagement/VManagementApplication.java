29bdac8d9ae73d55c33686d12dc574da36135a62
pratik_5498 <pratikdharme69@gmail.com>
2022-07-22 23:58:26 +0530

    FirstCommit

---
+++ VManagement/src/main/java/com/elixir/vmanagement/VManagementApplication.java
@@ -0,0 +1,13 @@
+package com.elixir.vmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VManagementApplication.class, args);
+	}
+
+}
---
+++ VManagement/src/test/java/com/elixir/vmanagement/VManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixir.vmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
